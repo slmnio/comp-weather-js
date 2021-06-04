7bb7e68877d30b6c7bb827c7827a3a5f06073af1
Solomon Cammack <solomon@solomon.eu.com>
2021-06-04 07:35:47 +0100

    Update body-parser and get a post ready

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const port = 8921;
 
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     console.log(":) nucleo board ping");
     res.send(`Hello Nucleo Board!`);
+})
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    res.send(":) thanks nucleo board");
 })
 
 app.listen(port, () => console.log(`comp-weather-js running at :${port}`));
